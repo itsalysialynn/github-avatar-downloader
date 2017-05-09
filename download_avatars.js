9ec0425a7f27abf322270606a792427cbb1704f3
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-09 03:16:09 +0000

    final commit - IT WORKS!

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,7 @@ var GITHUB_USER = "itsalysialynn";
 var GITHUB_TOKEN = "9d868a8f440729a0ab948fbfbadb9694358decae";
 
 var request = require('request');
-
+var fs = require("fs");
 
 function getRepoContributors(repoOwner, repoName, cb) {
 var options = {
@@ -11,7 +11,6 @@ var options = {
     'User-Agent': 'itsalysialynn'
   }
 };
- //console.log("testing", options);
  request(options, cb);
 
 
@@ -22,11 +21,17 @@ var options = {
 getRepoContributors(process.argv[2], process.argv[3], function(err, response, body) {
   var parsedResults = JSON.parse(body);
   for (var i = 0; i < parsedResults.length; i++) {
-  	console.log("avatar_url" body[avatar_url]);
+  	var avatarUrl =  parsedResults[i]["avatar_url"];
+  	downloadImageByURL(avatarUrl, `avatars/${i}.jpg`)
+
   }
-  //console.log("Errors:", err);
-  //console.log("Result:", parsedResults);
+ 
 });
+
+function downloadImageByURL(url, filePath) {
+	request(url).pipe(fs.createWriteStream(filePath));
+
+}
 
 
 
