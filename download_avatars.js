c83433acfc808646f1c69983978554e7ab93fa0c
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-10 22:42:55 +0000

    lint corrections

--- download_avatars.js
+++ download_avatars.js
@@ -5,33 +5,29 @@ var request = require('request');
 var fs = require("fs");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-var options = {
-  url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-  headers: {
-    'User-Agent': 'itsalysialynn'
-  }
-};
- request(options, cb);
+  var options = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'itsalysialynn'
+    }
+  };
 
+  request(options, cb);
+}
 
+function downloadImageByURL(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath));
 
- 
 }
 
 getRepoContributors(process.argv[2], process.argv[3], function(err, response, body) {
   var parsedResults = JSON.parse(body);
   for (var i = 0; i < parsedResults.length; i++) {
-  	var avatarUrl =  parsedResults[i]["avatar_url"];
-  	downloadImageByURL(avatarUrl, `avatars/${i}.jpg`)
-
+    var avatarUrl =  parsedResults[i]["avatar_url"];
+    downloadImageByURL(avatarUrl, `avatars/${i}.jpg`);
   }
- 
 });
 
-function downloadImageByURL(url, filePath) {
-	request(url).pipe(fs.createWriteStream(filePath));
-
-}
 
 
 
